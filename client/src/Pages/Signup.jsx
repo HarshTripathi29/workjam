5a28f4a802f04d119537914a628d3deeb9f8dd03
Harsh Tripathi <106900613+HarshTripathi29@users.noreply.github.com>
2024-06-23 10:19:30 +0530

    login

--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -1,13 +1,13 @@
 import React, {useState} from 'react'
 import {useNavigate} from "react-router-dom";
 import "./Signup.css";
-// import axios from "axios";
+import axios from "axios";
 
 const Signup = () => {
 
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [username, setUsername] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const[toggleSignup, setToggleSignup] = useState(true);
   const navigate = useNavigate();
@@ -16,54 +16,71 @@ const Signup = () => {
     setToggleSignup(!toggleSignup);
   }
 
+  const handleClick =async()=>{
+    try{
+    const response = await axios.post("http://localhost:3000/api/v1/auth/signup",{
+      firstname,
+      lastname,
+      email,
+      password
+    });
+    console.log(response.data);
+  }catch(error){
+    console.log("error signin up : ", error)
+  }
+  }
+
+  const handleSignin=async()=>{
+    try{
+      const response =await axios.post("http://localhost:3000/api/v1/auth/signin",{
+        email,
+        password
+      }); 
+
+      console.log(response.data);
+
+    }catch(error){
+      console.log("error signin in : ", error);
+    }
+  }
+
   return (
     <div className='signup-page'>
     <h2>{toggleSignup?"Sign Up" : "Sign In"}</h2>
     <p>Enter your deatils below</p>
       <div className='signup-form'>
-        <input type='email' 
-          placeholder='username'
-          onChange={e => {
-          setUsername(e.target.value);
-        }}/>
+        
         {toggleSignup &&(<input type='name' 
           placeholder='first name' 
+          value={firstname}
           onChange={e => {
-          setFirstName(e.target.value)}} />
+          setFirstname(e.target.value)}} />
         )}
         {toggleSignup &&(<input type='name ' 
           placeholder='last name' 
+          value={lastname}
           onChange={(e) => {
-          setLastName(e.target.value);
+          setLastname(e.target.value);
         }}/>
         )}
+        <input type='email' 
+          placeholder='email'
+          value={email}
+          onChange={e => {
+          setEmail(e.target.value);
+        }}/>
         <input type='password' 
           placeholder='password' 
+          value={password}
           onChange={(e) => {
           setPassword(e.target.value)
         }}/>
-       { toggleSignup &&( <button  onClick={async () => {
-            const response = await axios.post("http://localhost:3000/api/v1/user/signup", {
-              username,
-              firstName,
-              lastName,
-              password
-            });
-            localStorage.setItem("token", response.data.token)
-            navigate("/todo");
-         
-          }} > Signup </button>
+       { toggleSignup && ( 
+        <button onClick={handleClick}
+           > Signup </button>
          ) }
 
-         { !toggleSignup &&( <button  onClick={async () => {
-            // const response = await axios.post("http://localhost:3000/api/v1/user/signin", {
-            //   username,
-            //   password
-            // });
-            // localStorage.setItem("token", response.data.token)
-            // navigate("/todo");
-     
-          }} > Signin </button>
+         { !toggleSignup &&( <button  onClick={handleSignin} > Signin </button>
          ) }
            <div className='toggle-msg'>{toggleSignup ?"User already registered? " : "New here? "} <div className='toggle-btn' onClick={toggleForm}>{toggleSignup ? "Sign In" : "Sign Up"}</div></div>  
       </div>
