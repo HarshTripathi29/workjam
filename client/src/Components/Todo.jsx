a147f0722c695ec867980a87f1d3db0abad40fbd
Harsh Tripathi <106900613+HarshTripathi29@users.noreply.github.com>
2024-06-24 09:27:50 +0530

    todo

--- client/src/Components/Todo.jsx
+++ client/src/Components/Todo.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Todo.css';
 
 const Todo = () => {
+
+  const [todo, setTodo]=useState("");
+  const [list, setList]=useState([]);
+
+  const handleChange=(e)=>{
+    setTodo(e.target.value);
+    console.log(todo);
+  }
+
+  const handleClick=()=>{
+    setList([...list, todo]);
+    console.log(list);
+    setTodo('');
+    console.log(todo);
+  }
+
+  const handleDelete=(index)=>{
+    setList(list.filter((_, id)=>{
+      return id!==index;
+    }));
+  }
+
   return (
 
       <div className='todo'>
-        hello
+        <div className='todo-header'>
+          <input type='text' placeholder='create a todo' onChange={handleChange} value={todo}/>
+          <button onClick={handleClick}>+</button>
+        </div>
+        <div className='todo-body'>
+        <ul>
+          {
+            list.map((item, index)=>(
+              <div className='todo-item'>
+              <li key={index}>{item}</li>
+              <button onClick={()=>{handleDelete(index)}}>-</button>
+              </div>
+            ))
+          }
+          </ul>
+        </div>
       </div>
 
   )
